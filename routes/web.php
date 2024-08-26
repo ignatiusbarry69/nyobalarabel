94daa15c77b4212fd7e3ac6b9a8ba2d4ff0b1717
barry <ignatius.barry@gmail.com>
2024-08-26 16:28:28 +0700

    modularisasi ui dengan component

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title'=>'Home Page']);
 });
 
 
 Route::get('/about', function () {
-    return view('about',['nama' => 'Sayang']);
+    return view('about',['nama' => 'Sayang','title'=>'About']);
 });
 Route::get('/blog', function () {
-    return view('blog',['nama' => 'Sayang']);
+    return view('blog',['nama' => 'Sayang','title'=>'Blog']);
 });
 Route::get('/contact', function () {
-    return view('contact',['nama' => 'Sayang']);
+    return view('contact',['nama' => 'Sayang','title'=>'Contact']);
 });
